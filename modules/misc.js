00d9ee563179c1bd2b56d8e4481e40292f79ea8e
Julianne Stingray <flat.bartender@gmail.com>
2017-08-24 23:27:43 +0200

    Added help

--- modules/misc.js
+++ modules/misc.js
@@ -8,6 +8,27 @@ exports.commands = {
         callback: function (message) {
             message.reply("pong");
         }
+    },
+    "help": {
+        id: 2,
+        description: "I help you find all the commands you need!",
+        callback: async function (message) {
+            message.reply("I'm sending you the commands in DM!");
+            let help_lines = [];
+            help_lines.push("```");
+            Object.keys(global.modules).forEach((m) => {
+                let module = global.modules[m];
+                help_lines.push(`Module "${module.name}":`);
+                Object.keys(module.commands).forEach((key) => {
+                    help_lines.push(`\t"${key}": ${module.commands[key].description}`);
+                });
+            });
+
+            help_lines.push("If you have any bug report or feature request, please send me an email at flat.bartender@gensokyo.eu!");
+            help_lines.push("```");
+            let chan = await message.member.createDM();
+            chan.send(help_lines.join("\n"));
+        }
     }
 };
 exports.name = "misc";
